684bedc1ac81ed7636786c1c37e3e784f6324629
Riccardo Casatta <riccardo@casatta.it>
2021-04-21 15:17:19 +0200

    fix pre-bip34 duplicate

--- src/fee.rs
+++ src/fee.rs
@@ -7,6 +7,7 @@ use std::convert::TryInto;
 use std::sync::mpsc::Receiver;
 use std::sync::mpsc::SyncSender;
 use std::time::Instant;
+use bitcoin::hashes::hex::FromHex;
 
 pub struct Fee {
     skip_prevout: bool,
@@ -37,7 +38,14 @@ impl Utxo {
             txid.into(),
             tx.output.iter().map(|txout| Some(txout.clone())).collect(),
         );
-        assert!(prev.is_none(), "truncated hash caused a collision {}", txid);
+        if prev.is_some() {
+            // pre bip-34 issue, coinbase without height may create the same hash
+            if txid != Txid::from_hex("d5d27987d2a3dfc724e359870c6644b40e497bdc0589a033220fe15429d88599").unwrap() &&
+                txid != Txid::from_hex("e3bf3d07d4b0375638d5f1db5255fe07ba2c4cb067cd81b84ee974b6585fb468").unwrap() {
+                panic!("truncated hash caused a collision {}", txid);
+            }
+        }
+
         txid
     }
 
