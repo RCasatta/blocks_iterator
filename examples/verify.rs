c5ea985371103dbcc8bfebda3ce74d8c97d05ea7
Riccardo Casatta <riccardo@casatta.it>
2021-05-03 14:52:55 +0200

    verify in parallel

--- examples/verify.rs
+++ examples/verify.rs
@@ -4,7 +4,9 @@ use bitcoin::OutPoint;
 use blocks_iterator::{periodic_log_level, Config};
 use env_logger::Env;
 use log::{error, info, log};
+use rayon::prelude::*;
 use std::error::Error;
+use std::sync::atomic::{AtomicU64, AtomicUsize, Ordering};
 use std::sync::mpsc::sync_channel;
 use structopt::StructOpt;
 
@@ -15,7 +17,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let config = Config::from_args();
     let (send, recv) = sync_channel(100);
     let handle = blocks_iterator::iterate(config, send);
-    let mut err_count = 0;
+    let err_count = AtomicU64::new(0);
     while let Some(block_extra) = recv.recv()? {
         log!(
             periodic_log_level(block_extra.height),
@@ -24,14 +26,14 @@ fn main() -> Result<(), Box<dyn Error>> {
             block_extra.block_hash,
             block_extra.fee()
         );
-        for tx in block_extra.block.txdata.iter().skip(1) {
+        block_extra.block.txdata.par_iter().skip(1).for_each(|tx| {
             if let Err(e) = tx.verify_with_flags(
                 |point: &OutPoint| block_extra.outpoint_values.get(point).cloned(),
                 0u32,
             ) {
                 //TODO initializa flags correctly (now 0 is none)
                 error!(
-                    "err_{} {:?} {} {}",
+                    "err_{:?} {:?} {} {}",
                     err_count,
                     e,
                     tx.txid(),
@@ -43,16 +45,16 @@ fn main() -> Result<(), Box<dyn Error>> {
                         .get(&input.previous_output)
                         .unwrap();
                     error!(
-                        "err_{} input_{}: value:{} script_pubkey:{:x} script_sig:{:x}",
+                        "err_{:?} input_{}: value:{} script_pubkey:{:x} script_sig:{:x}",
                         err_count, i, prevout.value, prevout.script_pubkey, input.script_sig
                     );
                 }
-                err_count += 1;
-                panic!("");
+                err_count.fetch_add(1, Ordering::Relaxed);
+                //panic!("");
             }
-        }
+        });
     }
     handle.join().expect("couldn't join");
-    info!("error: {}", err_count);
+    info!("errors: {:?}", err_count);
     Ok(())
 }
