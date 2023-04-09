3908d51439c4132d8c303c89e428c84ac075bcd4
Riccardo Casatta <riccardo@casatta.it>
2023-04-09 21:53:52 +0200

    fix with rocksdb feature

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,10 @@ pub enum Error {
     #[error(transparent)]
     Redb(#[from] bitcoin_slices::redb::Error),
 
+    #[cfg(feature = "db")]
+    #[error(transparent)]
+    Rocksdb(#[from] rocksdb::Error),
+
     #[error("You can use only one db at a time")]
     OneDb,
 }
