d7fc3fe36a3d14f6660e2883f1a08fabd436e641
Riccardo Casatta <riccardo@casatta.it>
2023-11-26 11:42:19 +0100

    update example

--- examples/most_reused_address.rs
+++ examples/most_reused_address.rs
@@ -1,9 +1,11 @@
-use bitcoin::ScriptBuf;
+use bitcoin::{Address, Script, ScriptBuf};
 use blocks_iterator::Config;
 use env_logger::Env;
+use fxhash::FxHasher64;
 use log::info;
 use std::collections::HashMap;
 use std::error::Error;
+use std::hash::Hasher;
 use structopt::StructOpt;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -12,31 +14,37 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut config = Config::from_args();
     config.skip_prevout = true;
-    let mut m: HashMap<ScriptBuf, u64> = HashMap::new();
+    let mut m: HashMap<u64, u64> = HashMap::new();
+    let mut mapping_over_1000: HashMap<u64, ScriptBuf> = HashMap::new();
+    let network = config.network;
 
     for b in blocks_iterator::iter(config) {
-        for h in b
-            .iter_tx()
-            .into_iter()
-            .flat_map(|e| e.1.output.iter())
-            .map(|e| e.script_pubkey.clone())
-        {
-            *m.entry(h).or_default() += 1;
+        for tx_out in b.iter_tx().into_iter().flat_map(|e| e.1.output.iter()) {
+            let h = script_hash(&tx_out.script_pubkey);
+            let val = m.entry(h).or_default();
+            if *val > 1000 && !mapping_over_1000.contains_key(val) {
+                mapping_over_1000.insert(h, tx_out.script_pubkey.clone());
+            }
+
+            *val += 1;
         }
     }
 
     let mut vec: Vec<_> = m.into_iter().collect();
     vec.sort_by(|a, b| a.1.cmp(&b.1));
-
-    for a in vec.iter().rev().take(10) {
-        println!("{} {}", a.0, a.1);
-    }
-
-    println!();
+    println!("mapping over 1000 len: {}", mapping_over_1000.len());
 
     for a in vec.iter().take(10) {
-        println!("{} {}", a.0, a.1);
+        if let Some(script) = mapping_over_1000.get(&a.0) {
+            let address = Address::from_script(script, network)?;
+            println!("{} {}", address, a.1);
+        }
     }
-
     Ok(())
+}
+
+fn script_hash(script: &Script) -> u64 {
+    let mut hasher = FxHasher64::default();
+    hasher.write(script.as_bytes());
+    hasher.finish()
 }
